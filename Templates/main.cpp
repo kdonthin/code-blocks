00bc1a889456bd1285452d2e20fa48a79f00be10
Kishore Donthineni <kdonthineni@tnsi.com>
2014-10-30 16:43:41 -0400

    Added test1 method to test template function to iterate and print values in generic array.

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+template <typename T>
+void display(T item[], int size)
+{
+  for( int iCounter = 0 ; iCounter < size ; ++iCounter )
+  {
+    cout << item[iCounter] << " " ;
+  }
+}
+
+void test1()
+{
+  int ai[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  string ais[] = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"} ;
+
+  display(ai, 10) ;
+  cout << endl ;
+  display(ais, 10) ;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+  test1() ;
 
-    return 0;
+  return 0;
 }
